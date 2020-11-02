50073adb6444f13062db87cbb73cf0930d536b57
Dan <bush.daniel96@gmail.com>
2020-11-01 22:00:20 -0600

    Update hurtfulwords.py

--- hurtfulwords/hurtfulwords.py
+++ hurtfulwords/hurtfulwords.py
@@ -5,6 +5,7 @@ import lxml.html
 import lxml
 import random
 import requests
+from insult_list import insult_out
 
 class hurtfulwords(commands.Cog):
     #Insult your friends
@@ -14,7 +15,7 @@ class hurtfulwords(commands.Cog):
         #Define the command for RedBot
         message = (await ctx.channel.history(limit=2).flatten())[1].content
         if not message:
-            message = "I can't translate that!"
+            message = "I'm a dumb boy who can't think of insults"
         await type_message(
             ctx.channel,
             self.big_insults(message),
@@ -23,72 +24,5 @@ class hurtfulwords(commands.Cog):
         )
         
     @staticmethod
-    def big_insults(x):
-        return insult_list
-
-    letters = {         
-    'a':['  *  ',' * * ','*****','*   *','*   *'],
-    'b':['**** ','*   *','*****','*   *','**** '],
-    'c':[' ****','*    ','*    ','*    ',' ****'],
-    'd':['**** ','*   *','*   *','*   *','**** '],
-    'e':['*****','*    ','*****','*    ','*****'],
-    'f':['*****','*    ','*****','*    ','*    '],
-    'g':['*****','*    ','* ***','*   *','*****'],
-    'h':['*   *','*   *','*****','*   *','*   *'],
-    'i':['*****','  *  ','  *  ','  *  ','*****'],
-    'j':['***  ','  *  ','  *  ','  *  ','***  '],
-    'k':['*   *','* *  ','*    ','* *  ','*   *'],
-    'l':['*    ','*    ','*    ','*    ','*****'],
-    'm':['*   *','** **','* * *','*   *','*   *'],
-    'n':['*   *','**  *','* * *','*  **','*   *'],
-    'o':[' *** ','*   *','*   *','*   *',' *** '],
-    'p':['**** ','*   *','**** ','*    ','*    '],
-    'q':[' *** ','*   *','* * *','*  * ',' ** *'],
-    'r':['**** ','*   *','**** ','* *  ','*  **'],
-    's':[' ****','*    ',' *** ','    *','**** '],
-    't':['*****','  *  ','  *  ','  *  ','  *  '],
-    'u':['*   *','*   *','*   *','*   *',' *** '],
-    'v':['*   *','*   *',' * * ',' * * ','  *  '],
-    'w':['*   *','*   *','* * *','* * *',' * * '],
-    'x':['*   *',' * * ','  *  ',' * * ','*   *'],
-    'y':['*   *',' * * ','  *  ','  *  ','  *  '],
-    'z':['*****','   * ','  *  ',' *   ','*****'],
-    ',':['     ','     ','   **','   **','  *  '],
-    ':':['     ','  *  ','     ','  *  ','     '],
-    '|':['*****','*****','*****','*****','*****'],
-    ' ':['','','','','']
-    }
-
-    html = requests.get('http://www.insult.wiki/list-of-insults')
-    doc = lxml.html.fromstring(html.content)
-    insults_list = doc.xpath('//html/body/ol/li/a/@href')
-    insult_string = ''.join(insults_list)
-    remove_html = 'http://www.insult.wiki/insult/'
-    result = insult_string.replace(remove_html,' ')
-    final_list = []
-        # Large letter command
-
-    def big_letters(insult):
-        # Embed the letters in code block for discord
-        insult_out = ''
-        insult_out += ('```py\n')
-        # Print large format letters horizontally
-        for i in range(len(letters['|'])):  
-            for j in range(len(insult)):       
-                insult_out +=  letters[insult[j]][i] + "   "#end = " ")
-            insult_out += '\n'
-        #finish the code block
-        insult_out += ('```')
+    def big_insults(self):
         return insult_out
-        # Dictionary to call large format letters
-    def final(x):
-        final_list = []
-        for word in result.split(' '):
-            if len(word) <= 12:
-                final_list.append(word)
-            else:
-                pass
-        return random.choice(final_list)
-
-        # insult_list is the random insult big letter generator
-    insult_list = big_letters(final(final_list))
