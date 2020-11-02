acebeaf72d0cf8a156922196bd0aa3d4fbbf56d7
Dan <bush.daniel96@gmail.com>
2020-11-01 21:16:04 -0600

    Update bullybot.py

--- bullybot/bullybot.py
+++ bullybot/bullybot.py
@@ -89,30 +89,30 @@ letters = {
     '|':['*****','*****','*****','*****','*****'],
     ' ':['','','','','']
     }
-    
+
 html = requests.get('http://www.insult.wiki/list-of-insults')
 doc = lxml.html.fromstring(html.content)
 insults_list = doc.xpath('//html/body/ol/li/a/@href')
 insult_string = ''.join(insults_list)
 remove_html = 'http://www.insult.wiki/insult/'
 result = insult_string.replace(remove_html,' ')
+final_list = []
+    # Large letter command
 
-        # Large letter command
-
-    def big_letters(self, insult):
-            # Embed the letters in code block for discord
-        print('```py')
-            # Print large format letters horizontally
+def big_letters(self):
+        # Embed the letters in code block for discord
+        insult_out = ''
+        insult_out += ('```py\n')
+        # Print large format letters horizontally
         for i in range(len(letters['|'])):  
-            for j in range(len(insult)):       
-                print(letters[insult[j]][i] + "   ",end = " ")
-            print()
-            #finish the code block
-        print('```')
-        
+            for j in range(len(self)):       
+                insult_out +=  letters[self[j]][i] + "   "#end = " ")
+            insult_out += '\n'
+        #finish the code block
+        insult_out += ('```')
+        return insult_out
     # Dictionary to call large format letters
-    final_list = []
-    def final(self, x):
+def final(self):
         final_list = []
         for word in result.split(' '):
             if len(word) <= 12:
@@ -122,7 +122,7 @@ result = insult_string.replace(remove_html,' ')
         return random.choice(final_list)
 
         # insult_list is the random insult big letter generator
-    insult_list = big_letters(final(final_list))
+insult_list = big_letters(final(final_list))
         
 # Ideas for bully modules
 # Replace spaces with the poop emojii and reverse word order in all caps (added for now)
