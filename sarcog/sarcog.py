d0fe09561e868e444915292895b7ddf26680930a
Dan <bush.daniel96@gmail.com>
2020-11-06 15:12:11 -0600

    Update sarcog.py

--- sarcog/sarcog.py
+++ sarcog/sarcog.py
@@ -8,7 +8,7 @@ class sarcog(commands.Cog):
     @commands.command(aliases=["bully","b"])
     async def sarcasm(self, ctx: commands.Context):
         #Define the command for RedBot
-        message = (await ctx.channel.history(limit=2).flatten())[1].content
+        message = (await ctx.channel.history(limit=1).flatten())[0].content
         if not message:
             message = "I can't translate that!"
         await type_message(
