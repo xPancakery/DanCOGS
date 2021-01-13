b3607c374ef36a0207c0e712a8d01c7296dd8ef0
Dan <bush.daniel96@gmail.com>
2021-01-13 14:52:02 -0600

    Update eightball.py

--- eightball/eightball.py
+++ eightball/eightball.py
@@ -19,7 +19,7 @@ class magic8ball(commands.Cog):
             )
         
 
-    def qna(self, ctx, question)
+    def qna(self, ctx, question):
         choices = ['As I see it, yes.','Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.',
         "Don't count on it.",'It is certain.','It is decidedly so.','Most likely','My reply is no.',
         'My sources say no.','Outlook not so good.','Outlook good.','Reply hazy, try again.','Signs point to yes.',
