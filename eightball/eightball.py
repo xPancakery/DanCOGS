5803fdecd4e975531a6a31c2b41e6fa7cbd644e9
Dan <bush.daniel96@gmail.com>
2021-01-13 15:09:00 -0600

    Update eightball.py

--- eightball/eightball.py
+++ eightball/eightball.py
@@ -26,7 +26,7 @@ class magic_ball(commands.Cog):
         "Don't count on it.",'It is certain.','It is decidedly so.','Most likely','My reply is no.',
         'My sources say no.','Outlook not so good.','Outlook good.','Reply hazy, try again.','Signs point to yes.',
         'Very doubtful','Without a doubt.','Yes.','Yes - definitely.','You may rely on it.']
-        if '?' in *question[-1:]:
+        if '?' in question:
             answer = random.choice(choices)
             return(answer)
         else:
