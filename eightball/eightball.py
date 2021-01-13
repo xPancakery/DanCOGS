1c5bc0308b23b8aa57ef54de5a72eb67a44a5e7e
Dan <bush.daniel96@gmail.com>
2021-01-13 15:16:38 -0600

    Update eightball.py

--- eightball/eightball.py
+++ eightball/eightball.py
@@ -6,29 +6,20 @@ from redbot.core import commands
 
 class magic_ball(commands.Cog):
 
-    @commands.command(aliases=["8ball"])
+    @commands.command()
     async def magic_ball(self, ctx: commands.Context, question):
-        message = (await ctx.channel.history(limit=2).flatten())
+        message = (await ctx.channel.history(limit=2).flatten())[1].content
         if not message:
-            message = 'You must ask a question to the Magic 8 Ball. Example: .8ball"Is this a question?"'
+            message = "You must ask a question to the Magic 8 Ball."
         else:
-            await type_message(
-                ctx.channel,
-                self.qna(question),
-                allowed_mentions=discord.AllowedMentions(
-                    everyone=False, users=False, roles=False),
-            )
-        
-
-    def qna(self, question):
-        answer = ''
-        choices = ['As I see it, yes.','Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.',
+            choices = ['As I see it, yes.','Ask again later.','Better not tell you now.','Cannot predict now.','Concentrate and ask again.',
         "Don't count on it.",'It is certain.','It is decidedly so.','Most likely','My reply is no.',
         'My sources say no.','Outlook not so good.','Outlook good.','Reply hazy, try again.','Signs point to yes.',
         'Very doubtful','Without a doubt.','Yes.','Yes - definitely.','You may rely on it.']
         if '?' in question:
-            answer = random.choice(choices)
-            return(answer)
-#        else:
-#            return("Ask a question to the Magic 8 Ball.")
+            return(random.choice(choices))
+        else:
+            return("Use quotes, silly.")
+        
+        
         
